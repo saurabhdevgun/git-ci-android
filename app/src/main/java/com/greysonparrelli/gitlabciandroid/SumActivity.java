c1083bf2615194173cc2eea250c9a0b8b4db916b
Greyson Parrelli <greyson.parrelli@gmail.com>
2016-11-01 09:14:02 -0700

    Initial commit.

---
+++ app/src/main/java/com/greysonparrelli/gitlabciandroid/SumActivity.java
@@ -0,0 +1,16 @@
+package com.greysonparrelli.gitlabciandroid;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Greyson Parrelli (keybase.io/greyson)
+ */
+public class SumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
