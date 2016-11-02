0a06e54c4b8d61f0ef810cf5c70d8f1b80d196f2
Greyson Parrelli <greyson.parrelli@gmail.com>
2016-11-01 22:31:28 -0700

    Added a functional test.

---
+++ app/src/androidTest/java/com/greysonparrelli/gitlabciandroid/AddingTest.java
@@ -0,0 +1,46 @@
+package com.greysonparrelli.gitlabciandroid;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class AddingTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void addNumbers() throws Exception {
+        // Enter numbers to be added
+        onView(withId(R.id.num1))
+                .perform(typeText("1"), closeSoftKeyboard());
+        onView(withId(R.id.num2))
+                .perform(typeText("2"), closeSoftKeyboard());
+
+        // Click the calculate button
+        onView(withId(R.id.calc_button)).perform(click());
+
+        onView(withId(R.id.sum))
+                .check(matches(withText("3")));
+    }
+}
